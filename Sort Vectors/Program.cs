b4be545d86ed4547046b94731e3e41d5e1c31cff
Fabio Fattori <FABIO.FATTORI@local.ittcesena.it>
2020-10-07 11:06:41 +0200

    Firt step

---
+++ Sort Vectors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sort_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10];
+
+            Random r = new Random();
+
+            for (int i = 0; i < vet.Length; i++)
+                vet[i] = r.Next(int.MinValue,int.MaxValue);
+
+            foreach (int a in vet)
+                Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
