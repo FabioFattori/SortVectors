223358431c1835ff997e7bb40101e3c9f2908f15
Fabio Fattori <FABIO.FATTORI@local.ittcesena.it>
2020-10-07 11:09:47 +0200

    array ordinato di random

--- Sort Vectors/Program.cs
+++ Sort Vectors/Program.cs
@@ -13,6 +13,8 @@ namespace Sort_Vectors
             for (int i = 0; i < vet.Length; i++)
                 vet[i] = r.Next(int.MinValue,int.MaxValue);
 
+            Array.Sort(vet);
+
             foreach (int a in vet)
                 Console.WriteLine(a);
 
